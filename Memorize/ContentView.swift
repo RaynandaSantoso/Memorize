860ab0e55ef06f2931908419d11d3220ded99d03
Raynanda <raynandauni@gmail.com>
2024-11-24 20:37:39 +0700

    Added Buttons, Cleaned code for assignment 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,20 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: [String] = ["👻", "₿", "🎃", "🥶", "🤖", "😱", "🤡", "💀", "😴", "😈"]
+    @State var cardCount: Int = 4
+    
     var body: some View {
-        HStack {
-            let emojis: Array<String> = ["🤫", "₿", "🙂", "🥶"]
+        VStack {
+            cards
+            adjustCards
             
-            ForEach(emojis.indices, id: \.self) {index in
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) {index in
                 cardView(content: emojis[index])
             }
+            .foregroundColor(.orange)
+        }
+    }
+    
+    var adjustCards: some View {
+        HStack {
+            removeButton
+            Spacer()
+            addButton
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
             
-        }.foregroundColor(.orange)
-        
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var removeButton: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var addButton: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.fill.badge.plus")
     }
 }
-    
 
+
+
+    
 #Preview {
     ContentView()
 }
