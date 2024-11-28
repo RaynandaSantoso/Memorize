f0fdcad6069a05e25656fc65fdf139ee270c98f1
Raynanda <raynandauni@gmail.com>
2024-11-29 00:10:08 +0700

    Finished Assignment 1.
    Removed the "Add Card" button and added theme selector Buttons.
    Changed orientation with LazyVGrid.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,54 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: [String] = ["👻", "₿", "🎃", "🥶", "🤖", "😱", "🤡", "💀", "😴", "😈"]
-    @State var cardCount: Int = 4
+    let halloweenEmojis: [String] = ["👻", "₿", "🎃", "🥶", "🤖", "😱", "🤡", "💀", "😴", "😈","👻", "₿", "🎃", "🥶", "🤖", "😱", "🤡", "💀", "😴", "😈"]
+    let animalEmojis: [String] = ["😺", "🐞", "🐧", "🐷", "🐨", "🦁", "🐯", "🐻","😺", "🐞", "🐧", "🐷", "🐨", "🦁", "🐯", "🐻"]
+    let foodEmojis: [String] = ["🍓", "🍑", "🥕", "🍊","🍓", "🍑", "🥕", "🍊"]
+    
+    @State var selectedEmojis: [String] = []
     
     var body: some View {
         VStack {
+            Text("MEMORIZE!")
+                .font(.largeTitle)
+
             cards
-            adjustCards
-            
+            themeSelector
         }
         .padding()
     }
     
+    
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cardCount, id: \.self) {index in
-                cardView(content: emojis[index])
-            }
-            .foregroundColor(.orange)
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 60))]) {
+                ForEach(0..<selectedEmojis.count, id: \.self) {index in
+                    cardView(content: selectedEmojis[index])
+                        .aspectRatio(2/3, contentMode: .fit)
+                }
+                .foregroundColor(.orange)
         }
     }
-    
-    var adjustCards: some View {
-        HStack {
-            removeButton
-            Spacer()
-            addButton
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
-    
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+
+    func cardThemeSelector(emoji: [String], sysName: String, text: String) -> some View {
         Button(action: {
-            cardCount += offset
-            
+            selectedEmojis = emoji.shuffled()
         }, label: {
-            Image(systemName: symbol)
+            VStack {
+                Image(systemName: sysName)
+                    .font(.system(size: 30))
+                Text(text)
+            } .imageScale(.large)
         })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
     }
     
-    var removeButton: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    var themeSelector: some View {
+        HStack{
+            halloweenButton
+            Spacer()
+            animalButton
+            Spacer()
+            foodButton
+        }.padding()
+            .imageScale(.large)
     }
     
-    var addButton: some View {
-        cardCountAdjuster(by: 1, symbol: "rectangle.stack.fill.badge.plus")
+    var halloweenButton: some View {
+        cardThemeSelector(emoji: halloweenEmojis, sysName: "eye.circle.fill", text: "Halloween")
     }
+    var animalButton: some View {
+        cardThemeSelector(emoji: animalEmojis, sysName: "dog.circle.fill", text: "Animals")
+    }
+    
+    var foodButton: some View {
+        cardThemeSelector(emoji: foodEmojis, sysName: "takeoutbag.and.cup.and.straw.fill", text: "Foods")
+    }
+    
 }
 
 
@@ -75,15 +89,13 @@ struct cardView: View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
             
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
             }
-            else {
-                base.fill()
-            }
-            
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
             
         }.onTapGesture {
             isFaceUp.toggle()
